90e25b5fcba0839d0d0a6db0ecd8240bad48ab5a
joris devops <joris@devops.local>
2026-09-17 10:04:48 +0100

    Update application message

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class App {
 	public static void main(String[] args) {
-	 System.out.println("Hello TeamCity DevOps!");
+	 System.out.println("Hello TeamCity CI/CD DevOps!");
 	}
 }
